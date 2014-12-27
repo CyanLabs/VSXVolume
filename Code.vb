67818a6be4d3e6ca0d2081c907e960621885cd56
Fmna965 <fma965@cyanlabs.net>
2014-12-27 19:44:52 +0000

    Initial Commit

---
+++ Code.vb
@@ -0,0 +1,59 @@
+﻿Imports WindowsHookLib
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Module Module1
+    Dim ip
+    Public Sub Main()
+        kHook.InstallHook()
+        If Not My.Application.CommandLineArgs.Count = 0 Then
+            If IPAddress.TryParse(My.Application.CommandLineArgs(0), ip) Then
+                ep = New IPEndPoint(System.Net.IPAddress.Parse(My.Application.CommandLineArgs(0)), 8102)
+            Else
+                MsgBox("Enter a valid IP Address as the first command line argument", MsgBoxStyle.Exclamation, "Error")
+                Application.Exit()
+            End If
+        Else
+            MsgBox("Enter a valid IP Address as the first command line argument", MsgBoxStyle.Exclamation, "Error")
+            Application.Exit()
+        End If
+        Application.Run()
+    End Sub
+    Dim hostIp As IPAddress, serverIp As Byte()
+    Dim tnSocket As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+    Dim ep As IPEndPoint
+    Dim WithEvents kHook As New KeyboardHook
+    Private Function ConnectToVSX()
+        If tnSocket.Connected Then Return True
+        Try
+            tnSocket.Connect(ep)
+            If tnSocket.Connected Then Return True
+        Catch oEX As Exception
+            Return False
+        End Try
+        Return False
+    End Function
+
+    'Sends "cmd" to VSX.
+    Private Function SendCommands(ByVal cmd As String)
+        ConnectToVSX()
+        Dim SendBytes As [Byte]() = Encoding.ASCII.GetBytes(cmd & vbCrLf)
+        Dim NumBytes As Integer = 0
+        tnSocket.Send(SendBytes, SendBytes.Length, SocketFlags.None)
+        Return Nothing
+    End Function
+
+    Private Sub kHook_KeyDown(ByVal sender As Object, ByVal e As WindowsHookLib.KeyboardEventArgs) Handles kHook.KeyDown
+        If e.KeyCode = Keys.VolumeUp And Control.ModifierKeys = Keys.Control = False Then
+            e.Handled = True
+            SendCommands("VU")
+        ElseIf e.KeyCode = Keys.VolumeDown And Control.ModifierKeys = Keys.Control = False Then
+            e.Handled = True
+            SendCommands("VD")
+        ElseIf e.KeyCode = Keys.VolumeMute And Control.ModifierKeys = Keys.Control = False Then
+            e.Handled = True
+            SendCommands("MZ")
+        End If
+    End Sub
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VSX Volume")> 
+<Assembly: AssemblyDescription("Simple volume key hijack")> 
+<Assembly: AssemblyCompany("CyanLabs")> 
+<Assembly: AssemblyProduct("VSX Volume")> 
+<Assembly: AssemblyCopyright("Copyright © CyanLabs 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c3f42212-f615-426b-8b2d-7a516ba515a4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
