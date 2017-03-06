759a72eb679501dddecf73edda4f9bd1367142b6
fma965 <fma965@cyanlabs.net>
2017-03-06 23:53:07 +0000

    1.2
    Added OSD (optional, documentation coming soon)

--- Main.Designer.vb
+++ Main.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,22 +20,49 @@ Partial Class Main
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.lblOSD = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.SuspendLayout()
+        '
+        'lblOSD
+        '
+        Me.lblOSD.BackColor = System.Drawing.Color.Transparent
+        Me.lblOSD.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblOSD.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOSD.ForeColor = System.Drawing.Color.White
+        Me.lblOSD.Location = New System.Drawing.Point(0, 2)
+        Me.lblOSD.Name = "lblOSD"
+        Me.lblOSD.Size = New System.Drawing.Size(335, 59)
+        Me.lblOSD.TabIndex = 19
+        Me.lblOSD.Text = "VSX Volume" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Cyanlabs 2017"
+        Me.lblOSD.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblOSD.UseCompatibleTextRendering = True
+        Me.lblOSD.UseMnemonic = False
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(0, 0)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-        Me.Location = New System.Drawing.Point(1920, 1080)
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(335, 55)
+        Me.Controls.Add(Me.lblOSD)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Main"
+        Me.ShowIcon = False
         Me.ShowInTaskbar = False
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
         Me.Text = "Main"
+        Me.TopMost = True
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents lblOSD As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
 End Class
